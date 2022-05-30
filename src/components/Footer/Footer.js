e4f2b8b1c20abfba3f737bf7cbebf017eaa09acb
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-05-30 12:32:11 -0300

    terminar esqueleto

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,15 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="card">
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="/#" className="card-link">Card link</a>
-    <a href="/#" className="card-link">Another link</a>
-  </div>
-</div>
+    <h4 className="text-center p-5">Todos los derechos reservados a La Quiaqueña Herboristería | 2022</h4>
   );
 };
 
--- src/components/ProductosDestacados/ProductosDestacados.js
+++ src/components/ProductosDestacados/ProductosDestacados.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './ProductosDestacados.css';
 
 const ProductosDestacados = () => {
   return (
-    <div className="w-100 d-flex justify-content-evenly align-items-center">
-
+    <>
+    <h2 className="w-100 text-center pt-5">Productos Destacados</h2>
+    <div className="d-flex justify-content-evenly align-items-center my-5">
       <div className="w-25">
       <ul className="list-group list-group-flush">
   <li className="list-group-item">An item</li>
@@ -16,50 +18,53 @@ const ProductosDestacados = () => {
 
 <div className="w-50">
 <div className="row row-cols-1 row-cols-md-2 g-4">
-  <div className="col">
-    <div className="card">
+  <div className="card-producto w-25 d-flex justify-content-center p-0">
+    <div className="card w-75">
       <img src="https://picsum.photos/400/?random=5" className="card-img-top" alt="..." />
-      <div className="card-body">
+      <div className="card-body p-2">
         <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p className="card-text"></p>
         <button type="button" class="btn btn-outline-success">Success</button>
       </div>
     </div>
   </div>
-  <div className="col">
-    <div className="card">
+  <div className="card-producto w-25 d-flex justify-content-center p-0">
+    <div className="card w-75">
       <img src="https://picsum.photos/400/?random=6" className="card-img-top" alt="..." />
-      <div className="card-body">
+      <div className="card-body p-2">
         <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p className="card-text"></p>
         <button type="button" class="btn btn-outline-success">Success</button>
       </div>
     </div>
   </div>
-  <div className="col">
-    <div className="card">
+  <div className="card-producto w-25 d-flex justify-content-center p-0">
+    <div className="card w-75">
       <img src="https://picsum.photos/400/?random=7" className="card-img-top" alt="..." />
-      <div className="card-body">
+      <div className="card-body p-2">
         <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+        <p className="card-text"></p>
         <button type="button" class="btn btn-outline-success">Success</button>
       </div>
     </div>
   </div>
-  <div className="col">
-    <div className="card">
+  <div className="card-producto w-25 col d-flex justify-content-center p-0">
+    <div className="card w-75">
       <img src="https://picsum.photos/400/?random=8" className="card-img-top" alt="..." />
-      <div className="card-body">
+      <div className="card-body p-2">
         <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+        <p className="card-text"></p>
         <button type="button" class="btn btn-outline-success">Success</button>
       </div>
     </div>
   </div>
 </div>
 </div>
-
-    </div>
+</div>
+<div className="pb-4 px-5 d-flex aling-items-center">
+<button type="button" class="w-100 btn btn-success mb-0">Ver todos los productos</button>
+</div>
+    </>
   );
 };
 
