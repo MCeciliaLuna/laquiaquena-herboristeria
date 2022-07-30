8efec91a8ed94a3345c071b257538ac2934c7b9a
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-07-30 12:56:10 -0300

    banner agregado

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Carousel.css';
+import banner from '../assets/images/banner.png';
 
 const Carousel = () => {
   return (
     <div className="carousel">
     <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+    <img src={banner} className="banner-carousel" alt="logo" />
   <div className="carousel-inner carousel-padre">
     <div className="carousel-item active">
       <img src="https://images.pexels.com/photos/793012/pexels-photo-793012.jpeg?auto=compress&cs=tinysrgb&h=550&w=1060" className="d-block w-100" alt="..." />
