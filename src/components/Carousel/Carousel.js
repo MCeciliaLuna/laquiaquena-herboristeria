9c5a14d793bc9945fe1ea9cb71d58bd5ee586d30
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-08-01 17:36:52 -0300

    estilo inicial listo

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -9,13 +9,13 @@ const Carousel = () => {
     <img src={banner} className="banner-carousel" alt="logo" />
   <div className="carousel-inner carousel-padre">
     <div className="carousel-item active">
-      <img src="https://images.pexels.com/photos/793012/pexels-photo-793012.jpeg?auto=compress&cs=tinysrgb&h=550&w=1060" className="d-block w-100" alt="..." />
+      <img src="https://images.pexels.com/photos/268261/pexels-photo-268261.jpeg?auto=compress&cs=tinysrgb&h=550&w=1060" className="d-block w-100" alt="..." />
     </div>
     <div className="carousel-item">
       <img src="https://images.pexels.com/photos/268261/pexels-photo-268261.jpeg?auto=compress&cs=tinysrgb&h=550&w=1060" className="d-block w-100" alt="..." />
     </div>
     <div className="carousel-item">
-      <img src="https://images.pexels.com/photos/107606/pexels-photo-107606.jpeg?auto=compress&cs=tinysrgb&h=550&w=1060" className="d-block w-100" alt="..." />
+      <img src="https://images.pexels.com/photos/268261/pexels-photo-268261.jpeg?auto=compress&cs=tinysrgb&h=550&w=1060" className="d-block w-100" alt="..." />
     </div>
   </div>
 </div>
