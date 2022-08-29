617d7c45d746ed1f975c61914488b1e40561828e
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-08-29 17:00:46 -0300

    login style listo

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,19 +3,21 @@ import './Login.css';
 
 const login = () => {
   return (
-    <div className="login-page bg-light text-center">
-      <h2 className="pt-5 pb-2 d-block">Logueate</h2>
-      <form className="mt-0 mb-5 mx-5">
+    <div className="login-page bg-light text-center position-relative">
+      <div className="position-absolute top-50 start-50 translate-middle">
+      <h2 className="text-login fs-1 pt-0 pb-2 d-block text-light">Logueate</h2>
+      <form className="mt-0 mx-5">
   <div className="mb-3">
-    <label for="exampleInputEmail1" className="form-label w-100">Nombre de usuario</label>
-    <input type="email" className="input-login form-control w-50 m-2 text-center" id="exampleInputEmail1" aria-describedby="emailHelp" />
+    <label for="exampleInputEmail1" className="text-login fs-4 form-label w-100 text-light">Nombre de usuario</label>
+    <input type="text" className="w-100 m-2 text-center rounded-3 border-0 p-2" required/>
   </div>
   <div className="mb-3">
-    <label for="exampleInputPassword1" className="form-label">Contraseña</label>
-    <input type="password" className="input-login form-control w-50 m-2" id="exampleInputPassword1" />
+    <label for="exampleInputPassword1" className="text-login fs-4 form-label w-100 text-light">Contraseña</label>
+    <input type="password" className="w-100 m-2 text-center rounded-3 border-0 p-2" required />
   </div>
   <button type="submit" className="btn text-light">Entrar</button>
 </form>
+    </div>
     </div>
   );
 };
