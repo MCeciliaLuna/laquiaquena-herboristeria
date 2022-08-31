bc526d3c241ed99d57de237cff175e19ee525e14
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-08-31 15:44:05 -0300

    modales listos

--- src/pages/UsuarioLogueado/UsuarioLogueado.js
+++ src/pages/UsuarioLogueado/UsuarioLogueado.js
@@ -7,7 +7,7 @@ const UsuarioLogueado = () => {
     <div className="page-usuariologueado bg-light">
       <h2 className="text-center p-3">Page UsuarioLogueado</h2>
       <div className="d-flex justify-content-center">
-<button type="button" class="text-center btn text-light p-3" data-bs-toggle="modal" data-bs-target="#exampleModal">
+<button type="button" class="text-center btn text-light p-3 mb-4" data-bs-toggle="modal" data-bs-target="#exampleModal">
   AGREGAR NUEVO PRODUCTO
 </button>
 </div>
@@ -32,13 +32,13 @@ const UsuarioLogueado = () => {
 <div className="div-productos-page w-75 d-flex flex-wrap align-items-center justify-content-center">
   <div className="card-producto d-flex justify-content-center p-0 m-3 col-2">
     <div className="card text-center">
-      <img src="https://picsum.photos/400/?random=6" className="card-img-top" alt="..." />
+      <img src="https://picsum.photos/400/?random=55" className="card-img-top" alt="..." />
       <div className="card-body p-2">
         <h5 className="card-title m-0">Card title</h5>
         <p className="card-text py-2 m-0">Precio</p>
         <div className="text-center">
-        <button type="button" className="boton-productos-destacados btn bg-warning text-light mb-1" data-bs-toggle="modal" data-bs-target="#exampleModal">Editar</button>
-        <button type="button" className="btn bg-danger text-light mt-1" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <button type="button" className="boton-productos-destacados btn bg-warning text-light mb-1" data-bs-toggle="modal" data-bs-target="#exampleModal1">Editar</button>
+        <button type="button" className="btn bg-danger text-light mt-1" data-bs-toggle="modal" data-bs-target="#exampleModal2">
           Eliminar
         </button>
         </div>
@@ -47,13 +47,13 @@ const UsuarioLogueado = () => {
   </div>
   <div className="card-producto d-flex justify-content-center p-0 m-3 col-2">
     <div className="card text-center">
-      <img src="https://picsum.photos/400/?random=6" className="card-img-top" alt="..." />
+      <img src="https://picsum.photos/400/?random=56" className="card-img-top" alt="..." />
       <div className="card-body p-2">
         <h5 className="card-title m-0">Card title</h5>
         <p className="card-text py-2 m-0">Precio</p>
         <div className="text-center">
-        <button type="button" className="boton-productos-destacados btn bg-warning text-light mb-1" data-bs-toggle="modal" data-bs-target="#exampleModal">Editar</button>
-        <button type="button" className="btn bg-danger text-light mt-1" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <button type="button" className="boton-productos-destacados btn bg-warning text-light mb-1" data-bs-toggle="modal" data-bs-target="#exampleModal1">Editar</button>
+        <button type="button" className="btn bg-danger text-light mt-1" data-bs-toggle="modal" data-bs-target="#exampleModal2">
           Eliminar
         </button>
         </div>
@@ -62,13 +62,13 @@ const UsuarioLogueado = () => {
   </div>
   <div className="card-producto d-flex justify-content-center p-0 m-3 col-2">
     <div className="card text-center">
-      <img src="https://picsum.photos/400/?random=6" className="card-img-top" alt="..." />
+      <img src="https://picsum.photos/400/?random=57" className="card-img-top" alt="..." />
       <div className="card-body p-2">
         <h5 className="card-title m-0">Card title</h5>
         <p className="card-text py-2 m-0">Precio</p>
         <div className="text-center">
-        <button type="button" className="boton-productos-destacados btn bg-warning text-light mb-1" data-bs-toggle="modal" data-bs-target="#exampleModal">Editar</button>
-        <button type="button" className="btn bg-danger text-light mt-1" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <button type="button" className="boton-productos-destacados btn bg-warning text-light mb-1" data-bs-toggle="modal" data-bs-target="#exampleModal1">Editar</button>
+        <button type="button" className="btn bg-danger text-light mt-1" data-bs-toggle="modal" data-bs-target="#exampleModal2">
           Eliminar
         </button>
         </div>
@@ -77,13 +77,13 @@ const UsuarioLogueado = () => {
   </div>
   <div className="card-producto d-flex justify-content-center p-0 m-3 col-2">
     <div className="card text-center">
-      <img src="https://picsum.photos/400/?random=6" className="card-img-top" alt="..." />
+      <img src="https://picsum.photos/400/?random=58" className="card-img-top" alt="..." />
       <div className="card-body p-2">
         <h5 className="card-title m-0">Card title</h5>
         <p className="card-text py-2 m-0">Precio</p>
         <div className="text-center">
-        <button type="button" className="boton-productos-destacados btn bg-warning text-light mb-1" data-bs-toggle="modal" data-bs-target="#exampleModal">Editar</button>
-        <button type="button" className="btn bg-danger text-light mt-1" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <button type="button" className="boton-productos-destacados btn bg-warning text-light mb-1" data-bs-toggle="modal" data-bs-target="#exampleModal1">Editar</button>
+        <button type="button" className="btn bg-danger text-light mt-1" data-bs-toggle="modal" data-bs-target="#exampleModal2">
           Eliminar
         </button>
         </div>
@@ -92,13 +92,13 @@ const UsuarioLogueado = () => {
   </div>
   <div className="card-producto d-flex justify-content-center p-0 m-3 col-2">
     <div className="card text-center">
-      <img src="https://picsum.photos/400/?random=6" className="card-img-top" alt="..." />
+      <img src="https://picsum.photos/400/?random=59" className="card-img-top" alt="..." />
       <div className="card-body p-2">
         <h5 className="card-title m-0">Card title</h5>
         <p className="card-text py-2 m-0">Precio</p>
         <div className="text-center">
-        <button type="button" className="boton-productos-destacados btn bg-warning text-light mb-1" data-bs-toggle="modal" data-bs-target="#exampleModal">Editar</button>
-        <button type="button" className="btn bg-danger text-light mt-1" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <button type="button" className="boton-productos-destacados btn bg-warning text-light mb-1" data-bs-toggle="modal" data-bs-target="#exampleModal1">Editar</button>
+        <button type="button" className="btn bg-danger text-light mt-1" data-bs-toggle="modal" data-bs-target="#exampleModal2">
           Eliminar
         </button>
         </div>
@@ -107,13 +107,13 @@ const UsuarioLogueado = () => {
   </div>
   <div className="card-producto d-flex justify-content-center p-0 m-3 col-2">
     <div className="card text-center">
-      <img src="https://picsum.photos/400/?random=6" className="card-img-top" alt="..." />
+      <img src="https://picsum.photos/400/?random=60" className="card-img-top" alt="..." />
       <div className="card-body p-2">
         <h5 className="card-title m-0">Card title</h5>
         <p className="card-text py-2 m-0">Precio</p>
         <div className="text-center">
-        <button type="button" className="boton-productos-destacados btn bg-warning text-light mb-1" data-bs-toggle="modal" data-bs-target="#exampleModal">Editar</button>
-        <button type="button" className="btn bg-danger text-light mt-1" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <button type="button" className="boton-productos-destacados btn bg-warning text-light mb-1" data-bs-toggle="modal" data-bs-target="#exampleModal1">Editar</button>
+        <button type="button" className="btn bg-danger text-light mt-1" data-bs-toggle="modal" data-bs-target="#exampleModal2">
           Eliminar
         </button>
         </div>
@@ -122,13 +122,13 @@ const UsuarioLogueado = () => {
   </div>
   <div className="card-producto d-flex justify-content-center p-0 m-3 col-2">
     <div className="card text-center">
-      <img src="https://picsum.photos/400/?random=6" className="card-img-top" alt="..." />
+      <img src="https://picsum.photos/400/?random=61" className="card-img-top" alt="..." />
       <div className="card-body p-2">
         <h5 className="card-title m-0">Card title</h5>
         <p className="card-text py-2 m-0">Precio</p>
         <div className="text-center">
-        <button type="button" className="boton-productos-destacados btn bg-warning text-light mb-1" data-bs-toggle="modal" data-bs-target="#exampleModal">Editar</button>
-        <button type="button" className="btn bg-danger text-light mt-1" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <button type="button" className="boton-productos-destacados btn bg-warning text-light mb-1" data-bs-toggle="modal" data-bs-target="#exampleModal1">Editar</button>
+        <button type="button" className="btn bg-danger text-light mt-1" data-bs-toggle="modal" data-bs-target="#exampleModal2">
           Eliminar
         </button>
         </div>
@@ -137,13 +137,13 @@ const UsuarioLogueado = () => {
   </div>
   <div className="card-producto d-flex justify-content-center p-0 m-3 col-2">
     <div className="card text-center">
-      <img src="https://picsum.photos/400/?random=6" className="card-img-top" alt="..." />
+      <img src="https://picsum.photos/400/?random=62" className="card-img-top" alt="..." />
       <div className="card-body p-2">
         <h5 className="card-title m-0">Card title</h5>
         <p className="card-text py-2 m-0">Precio</p>
         <div className="text-center">
-        <button type="button" className="boton-productos-destacados btn bg-warning text-light mb-1" data-bs-toggle="modal" data-bs-target="#exampleModal">Editar</button>
-        <button type="button" className="btn bg-danger text-light mt-1" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <button type="button" className="boton-productos-destacados btn bg-warning text-light mb-1" data-bs-toggle="modal" data-bs-target="#exampleModal1">Editar</button>
+        <button type="button" className="btn bg-danger text-light mt-1" data-bs-toggle="modal" data-bs-target="#exampleModal2">
           Eliminar
         </button>
         </div>
@@ -159,7 +159,7 @@ const UsuarioLogueado = () => {
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Nuevo producto</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
@@ -171,6 +171,41 @@ const UsuarioLogueado = () => {
     </div>
   </div>
 </div>
+<div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Editar</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn text-light">Guardar</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Eliminar</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn bg-danger text-light">Eliminar</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
     </div>
   );
 };
