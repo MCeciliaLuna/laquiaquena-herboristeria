f93149f41d3cba33d6a43eb4862b23c04ca59f4a
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-09-29 19:57:35 -0300

    resolvi problemas de DB

--- src/components/CardProductoVenta/CardProductoVenta.js
+++ src/components/CardProductoVenta/CardProductoVenta.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const CardProductoVenta = ({productos = []}) => {
+  
+
   return (
     <div className="d-flex flex-wrap align-items-center justify-content-center">
       {
@@ -36,6 +38,121 @@ const CardProductoVenta = ({productos = []}) => {
                 </div>
               </div>
             </div>
+
+            <div
+        className="modal fade"
+        id="exampleModal1"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Editar
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form>
+                <div className="mb-3">
+                  <label for="disabledTextInput" className="fs-5 form-label">
+                    Nombre
+                  </label>
+                  <input
+                    type="text"
+                    id="disabledTextInput"
+                    className="form-control"
+                    placeholder="Nombre existente"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label for="disabledSelect" className="fs-5 form-label">
+                    Categoría
+                  </label>
+                  <select id="disabledSelect" className="form-select">
+                    <option>Categoría 1</option>
+                    <option>Categoría 2</option>
+                    <option>Categoría 3</option>
+                    <option>Categoría 4</option>
+                  </select>
+                </div>
+                <div className="mb-3">
+                  <label for="disabledTextInput" className="fs-5 form-label">
+                    Precio
+                  </label>
+                  <div className="d-flex">
+                    <p className="fs-5 m-1 pe-2">$</p>
+                    <input
+                      type="number"
+                      id="disabledTextInput"
+                      className="form-control w-50"
+                      placeholder="1234"
+                    />
+                  </div>
+                </div>
+                <div className="mb-3">
+                  <label className="fs-5 mb-1">Subir foto</label>
+                  <input
+                    type="file"
+                    className="p-1 d-block rounded-3 w-100"
+                    id=""
+                  />
+                </div>
+                <div className="modal-footer">
+                  <button type="button" className="btn text-light">
+                    Modificar producto
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className="modal fade"
+        id="exampleModal2"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Eliminar
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <h4>
+                ¿Estás segur@ de eliminar 
+                <b className="text-danger">{item.nombre}</b>?
+              </h4>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn boton-logueado-eliminar text-light"
+              >
+                Eliminar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
           </div>
         ))
       }
