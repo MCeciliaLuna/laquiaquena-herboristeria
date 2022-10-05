c7fb19204fda75be477a2edbf0fb04de80e707d7
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-10-05 20:06:27 -0300

    carousel arreglado

--- src/components/CardProductoPublico/CardProductoPublico.js
+++ src/components/CardProductoPublico/CardProductoPublico.js
@@ -20,7 +20,7 @@ const CardProductoPublico = ({productos = []}) => {
                 <p className="card-text py-2 m-0">{item.descripcion}</p>
                 <h5 className="card-text py-2 m-0"> $ {item.precio}</h5>
                 <div className="text-center">
-                  <a href={`https://api.whatsapp.com/send?phone=543816427068&text=Informacion%20sobre%20${item.nombre}`} target="new" >
+                  <a href={`https://api.whatsapp.com/send?phone=5493816427068&text=%C2%A1Hola%20*La%20Quiaque%C3%B1a%20Herborister%C3%ADa*%20%F0%9F%91%8B%F0%9F%8C%BF%20!%20Visit%C3%A9%20su%20p%C3%A1gina%20web%20y%20quisiera%20informaci%C3%B3n%20del%20producto%20*${item.nombre}*.%20Muchas%20gracias!%20%E2%98%BA`} target="new" >
                   <button
                     type="button"
                     className="btn text-light mb-1"
