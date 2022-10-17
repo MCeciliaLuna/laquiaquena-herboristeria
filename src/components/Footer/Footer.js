90d44cbe646e95a2bc417e066dba3be346e5ca14
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-10-17 16:41:50 -0300

    link a login de a a Link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Footer.css';
 
 const Footer = () => {
   return (
     <div className="footer d-block ">
     <h4 className="text-center mt-4 text-light pt-5">Todos los derechos reservados a La Quiaqueña Herboristería | 2022</h4>
-    <a href="/login" className="text-light text-center m-0">
+    <Link to="/login" className="text-light text-center m-0">
       <p className="linklogueo">Logueo de Administrador</p>
-      </a>
+      </Link>
     </div>
   );
 };
