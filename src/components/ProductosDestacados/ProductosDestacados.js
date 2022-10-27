85af2b2e9eae2e2afd04deb8f5627facbd87dba4
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-10-27 14:41:41 -0300

    destacados, responsive listo

--- src/components/ProductosDestacados/ProductosDestacados.js
+++ src/components/ProductosDestacados/ProductosDestacados.js
@@ -29,8 +29,8 @@ const ProductosDestacados = () => {
 <div className="div-margin-moviles g-4 d-flex justify-content-center">
 {
    productosFiltrados.map(producto => (
-    <div className="card-producto d-flex justify-content-center p-0 m-3 col-2" key={producto._id}>
-            <div className="card text-center">
+    <div className="card-producto d-flex justify-content-center p-0 m-3 col-2 align-items-around" key={producto._id}>
+            <div className="card card-productodestacado text-center">
               <div className="card-image-producto">
               <img
                 src={producto.image.secure_url}
@@ -39,18 +39,15 @@ const ProductosDestacados = () => {
               /> 
               </div>
               <div className="card-destacados card-body p-2">
-                <h4 className="titulo-destacados card-title mt-3 pt-1 mb-0 d-flex justify-content-center align-items-center">{producto.nombre}</h4>
-                <p className="descripcion-destacados card-text m-0 p-1 d-flex justify-content-center align-items-center">{producto.descripcion}</p>
-                {/* <h5 className="card-text py-2 m-0"> $ {producto.precio}</h5> */}
-                <div className="text-center">
+                <h4 className="titulo-destacados card-title mx-1 mb-0 d-flex justify-content-center align-items-center">{producto.nombre}</h4>
+                <p className="descripcion-destacados card-text m-0 p-0 d-flex justify-content-center align-items-center">{producto.descripcion}</p>
                   <a href={`https://api.whatsapp.com/send?phone=5493812183467&text=%C2%A1Hola%20*La%20Quiaque%C3%B1a%20Herborister%C3%ADa*%20%F0%9F%91%8B%F0%9F%8C%BF%20!%20Visit%C3%A9%20su%20p%C3%A1gina%20web%20y%20quisiera%20informaci%C3%B3n%20del%20producto%20*${producto.nombre}*.%20Muchas%20gracias!%20%E2%98%BA`} target="new" >
                   <button
                     type="button"
-                    className="btn button-destacados-comprar text-light m-1"
+                    className="btn button-destacados-comprar text-light mt-3 align-items-end"
                   > 
                     Comprar
                   </button> </a>
-                </div>
               </div>
             </div>
           </div>
