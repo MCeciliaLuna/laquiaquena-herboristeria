03d0678cc142a2b8f5b02516a59c2acbf7def8ef
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-10-27 15:39:12 -0300

    pagina lista

--- src/components/CardProductoVenta/CardProductoVenta.js
+++ src/components/CardProductoVenta/CardProductoVenta.js
@@ -32,31 +32,22 @@ const CardProductoVenta = ({productos = []}) => {
     <div className="d-flex flex-wrap align-items-center justify-content-center">
             {
               productosOrdenados.map((item, index)=>
-              <div className="card-producto d-flex justify-content-center p-0 m-3 col-2">
-              <div className="card text-center">
+              <div className="card-producto d-flex justify-content-center p-0 m-3 col-2 align-items-around">
+              <div className="card text-center card-productoventa">
               <img
                 src={item.image.secure_url}
                 className="card-img-top"
                 alt="nombre"
               />
               <div className="card-body p-2">
-                <h4 className="cardventa-titulo d-flex justify-content-center align-items-center card-title mt-3 pt-1 mb-0">{item.nombre}</h4>
-                <p className="cardventa-descripcion d-flex justify-content-center align-items-center card-text m-0 p-1">{item.descripcion}</p>
-                {/* <h5 className="card-text py-2 m-0"> ${item.precio}</h5> */}
+                <h4 className="cardventa-titulo d-flex justify-content-center align-items-center card-title my-2 mb-0">{item.nombre}</h4>
+                <p className="cardventa-descripcion d-flex justify-content-center align-items-center card-text m-0 p-0">{item.descripcion}</p>
                 
                   <div className="d-block">
-                  {/* <button
-                    type="button"
-                    className="boton-logueado-editar btn text-light m-1"
-                    data-bs-toggle="modal"
-                    data-bs-target="#exampleModal1"
-                  >
-                    Editar
-                  </button> */}
                   <button
                     type="button"
                     id="botonEliminar"
-                    className="btn boton-logueado-eliminar text-light mx-1"
+                    className="btn boton-logueado-eliminar text-light mt-3"
                     data-bs-toggle="modal"
                     data-bs-target="#exampleModal2"
                     onClick={(_id) => eliminarProducto(item._id)}
