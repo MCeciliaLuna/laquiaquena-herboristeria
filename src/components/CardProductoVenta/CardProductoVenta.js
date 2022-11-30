a2e611d68178497014f0d7ea1408713701e41a7f
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-11-30 12:51:13 -0300

    modal editar listo

--- src/components/CardProductoVenta/CardProductoVenta.js
+++ src/components/CardProductoVenta/CardProductoVenta.js
@@ -45,7 +45,6 @@ const CardProductoVenta = ({productos = []}) => {
                 <p className="cardventa-descripcion fs-6 d-flex justify-content-center align-items-center card-text">{item.descripcion}</p>
                 <h5>$ {item.precio}</h5>
                 
-                  <div className="d-block">
                   <button
                     type="button"
                     id="botonEliminar"
@@ -54,17 +53,16 @@ const CardProductoVenta = ({productos = []}) => {
                     data-bs-target="#exampleModal2"
                     onClick={(_id) => eliminarProducto(item._id)}
                   >
-                    Eliminar
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+  <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+</svg>
                   </button>
                 </div>
                 </div>
               </div>
-            </div>
-            
               )
             }
-
-            <ModalEditar />
+<ModalEditar />
 
           </div>
   );
--- src/components/ModalEditar/ModalEditar.js
+++ src/components/ModalEditar/ModalEditar.js
@@ -1,39 +1,22 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useForm } from "react-hook-form";
+import './ModalEditar.css'
 
 const ModalEditar = ({_id}) => {
     const { register, handleSubmit } = useForm();
-    const [ nombre, setNombre ] = useState('')
-    const [ descripcion, setDescripcion ] = useState('')
-    const [ precio, setPrecio ] = useState('')
-    // const [ image, setImage ] = useState('')
-    const [ id, setId ] = useState('')
 
-    const getProductoById = async () => {
-      const res = await axios.get('https://laquiaquenaherboristeriabe.onrender.com/traerproductos')
-      setNombre(res.data.nombre)
-      setDescripcion(res.data.descripcion)
-      setPrecio(res.data.precio)
-      // setImage(res.data.image)
-      setId(res.data._id)
+  const editarProducto = (data) => {
+   console.log(data)
     }
 
-  const editarProducto = async (e, _id) => {
-    e.preventDefault()
-    await axios.put(`https://laquiaquenaherboristeriabe.onrender.com/eliminarproducto/${_id}`,{
-      nombre: nombre,
-      descripcion: descripcion,
-      precio: precio
-      // image: image
-    })
-  }
-
-  useEffect(()=> {
-    getProductoById()
-  }, [])
-  
   return (
+    <div>
+    <button type="button" className="btn" data-bs-toggle="modal" data-bs-target="#exampleModal1">
+    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-pencil-fill text-light" viewBox="0 0 16 16">
+  <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+</svg>
+</button>
     <div
         className="modal fade"
         id="exampleModal1"
@@ -42,9 +25,9 @@ const ModalEditar = ({_id}) => {
         aria-hidden="true"
       >
         <div className="modal-dialog">
-          <div className="modal-content">
+          <div className="modal-content fondo-modal-editar">
             <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel1">
+              <h5 className="modal-title text-light" id="exampleModalLabel1">
                 Editar
               </h5>
               <button
@@ -55,71 +38,89 @@ const ModalEditar = ({_id}) => {
               ></button>
             </div>
             <div className="modal-body">
-              <form  onSubmit={handleSubmit(editarProducto)}>
-                <div className="mb-3">
-                  <label for="disabledTextInput" className="fs-5 form-label">
-                    Nombre
-                  </label>
-                  <input
-                    type="text"
-                    id="disabledTextInput"
-                    className="form-control"
-                    value={nombre}
-                    onChange={(e) => setNombre(e.target.value)}
-                    {...register("descripcion", { require: true })}
-                    required
-                  />
-                </div>
-                <div className="mb-3">
-                  <label for="disabledTextInput" className="fs-5 form-label">
-                    Descripción
-                  </label>
-                  <input
-                    type="text"
-                    id="disabledTextInput"
-                    className="form-control"
-                    value={descripcion}
-                    onChange={(e) => setDescripcion(e.target.value)}
-                    {...register("descripcion", { require: true })}
-                    required
-                  />
-                </div>
-                <div className="mb-3">
-                  <label for="disabledTextInput" className="fs-5 form-label">
-                    Precio
-                  </label>
-                  <div className="d-flex">
-                    <p className="fs-5 m-1 pe-2">$</p>
-                    <input
-                      type="number"
-                      id="disabledTextInput"
-                      className="form-control w-50"
-                      value="precio"
-                      onChange={(e) => setPrecio(e.target.value)}
-                      {...register("precio", { require: true })}
-                    required
-                    />
-                  </div>
-                </div>
-                <div className="mb-3">
-                  <label className="fs-5 mb-1">Subir foto</label>
-                  {/* <input
-                    type="file"
-                    className="p-1 d-block rounded-3 w-100"
-                    id=""
-                    {...register("image", { require: true })}
-                    required
-                  /> */}
-                </div>
-                <div className="modal-footer">
-                  <button type="submit" className="btn text-light">
-                    Modificar producto
-                  </button>
-                </div>
-              </form>
+            <form id="form" onSubmit={handleSubmit(editarProducto)}>
+          <div className="mb-3">
+            <label for="disabledTextInput" className="fs-5 form-label text-light">
+              Nombre
+            </label>
+            <input
+              type="text"
+              id="disabledTextInput"
+              className="form-control input-nombreproducto"
+              maxLength="40"
+              placeholder="ingresá tipo + nombre"
+              {...register("nombre", { required: true })} required
+            />
+          </div>
+          <div className="mt-4 mb-3">
+          <select className="form-select" aria-label="Default select example" {...register("categoria", { required: true })}required>
+  <option selected>Seleccioná una categoría</option>
+  <option value="Stevia">Stevia</option>
+  <option value="Hierbas">Hierbas</option>
+  <option value="Maca">Maca</option>
+  <option value="Cápsulas">Cápsulas</option>
+  <option value="Frascos de vidrio">Frascos de vidrio</option>
+  <option value="Nuez de la india">Nuez de la india</option>
+  <option value="En sobre">En sobre</option>
+  <option value="Pomadas">Pomadas</option>
+  <option value="Jarabe">Jarabe</option>
+  <option value="Jabones">Jabones</option>
+  <option value="Té">Té</option>
+  <option value="Aceites">Aceites</option>
+  <option value="Sahumerios">Sahumerios</option>
+  <option value="Otros">Otros</option>
+</select>
+</div>
+          <div className="mb-3">
+            <label for="disabledTextInput" className="fs-5 form-label text-light">
+              Descripción
+            </label>
+            <textarea
+              type="text"
+              id="disabledTextInput"
+              className="form-control"
+              maxLength="100"
+              placeholder="agregá tipo + propiedades brevemente"
+              rows="3"
+              {...register("descripcion", { required: true })} required
+            />
+          </div>
+          <div className="mb-3">
+            <label for="disabledTextInput" className="fs-5 form-label text-light">
+              Precio
+            </label>
+            <div className="d-flex">
+          <div className="d-flex align-items-center">
+            <p className="text-light fs-5 m-2">$</p>
+          <input
+              type="number"
+              id="disabledTextInput"
+              className="form-control input-nombreproducto"
+              maxLength="10"
+              placeholder="ingresá el precio"
+              {...register("precio", { required: true })} required
+            />
+            </div>  
+            </div>
+          </div>
+           <div className="mb-3">
+            <label className="fs-5 mb-1 text-light">Subir foto (debe ser cuadrada)</label>
+            <input
+              type="file"
+              className="btn p-1 d-block rounded-3 w-100"
+              {...register("image", { required: true })} required
+             />
+          </div>
+          <div className="modal-footer">
+            <button type="submit" className="btn text-light">
+              Modificar producto
+            </button>
+          </div>
+        </form>
             </div>
           </div>
         </div>
+      </div>
       </div>
   );
 };
