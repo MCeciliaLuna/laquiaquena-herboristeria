2192629d8822fea22584e977e3693cb37b0aa724
MCeciliaLuna <lunama.cecilia@gmail.com>
2023-03-13 10:36:13 -0300

    Barra de categorías estilizada

--- src/components/LinksCategoriasUser/LinksCategoriasUser.js
+++ src/components/LinksCategoriasUser/LinksCategoriasUser.js
@@ -5,22 +5,22 @@ const LinksCategoriasUser = ({ setSelectedCategory }) => {
 
   return (
     <div className="d-flex justify-content-center align-items-center mt-4 py-4 flex-wrap links-admin-categoria">
-    <button className="text-light mx-2 link-admin-categoria" onClick={() => setSelectedCategory('all')}>Todos los productos</button>
-    <button className="text-light mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Aceites')} >Aceites</button>
-    <button className="text-light mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Capsulas')}>Cápsulas</button>
-    <button className="text-light mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Ensobre')}>En sobre</button>
-    <button className="text-light mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Frascos')}>Frascos de vidrio</button>
-    <button className="text-light mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Hierbas')}>Hierbas</button>
-    <button className="text-light mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Jabones')}>Jabones</button>
-    <button className="text-light mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Jarabe')}>Jarabe</button>
-    <button className="text-light mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Maca')}>Maca</button>
-    <button className="text-light mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Nuezdelaindia')}>Nuez de la India</button>
-    <button className="text-light mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Pomadas')}>Pomadas</button>
-    <button className="text-light mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Sahumerios')}>Sahumerios</button>
-    <button className="text-light mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Stevia')} >Stevia</button>
-    <button className="text-light mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Te')}>Té</button>
-    <button className="text-light mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Yerbas')}>Yerbas</button>
-    <button className="text-light mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Otros')}>Otros</button>
+    <p className="text-light my-0 mx-2 link-admin-categoria" onClick={() => setSelectedCategory('all')}>Todos los productos</p>
+    <p className="text-light my-0 mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Aceites')} >Aceites</p>
+    <p className="text-light my-0 mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Capsulas')}>Cápsulas</p>
+    <p className="text-light my-0 mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('En sobre')}>En sobre</p>
+    <p className="text-light my-0 mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Frascos')}>Frascos de vidrio</p>
+    <p className="text-light my-0 mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Hierbas')}>Hierbas</p>
+    <p className="text-light my-0 mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Jabones')}>Jabones</p>
+    <p className="text-light my-0 mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Jarabe')}>Jarabe</p>
+    <p className="text-light my-0 mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Maca')}>Maca</p>
+    <p className="text-light my-0 mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Nuez de la India')}>Nuez de la India</p>
+    <p className="text-light my-0 mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Pomadas')}>Pomadas</p>
+    <p className="text-light my-0 mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Sahumerios')}>Sahumerios</p>
+    <p className="text-light my-0 mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Stevia')} >Stevia</p>
+    <p className="text-light my-0 mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Te')}>Té</p>
+    <p className="text-light my-0 mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Yerbas')}>Yerbas</p>
+    <p className="text-light my-0 mx-2 link-admin-categoria"  onClick={() => setSelectedCategory('Otros')}>Otros</p>
       </div>
   );
 };
